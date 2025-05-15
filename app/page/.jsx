7d49027becc5c8d7e3372d9deca39ad0b1c13975
dudu2612 <dkhairi50@gmail.com>
2025-05-16 02:32:15 +0800

    Create .jsx

---
+++ app/page/.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Selamat datang, Juniper!</h1>
+      <p>Project-mu berhasil tampil 🎉</p>
+    </div>
+  );
+}
