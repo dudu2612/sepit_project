447df4e8fe9b2397728cdef12ce586c4b14153f3
dudu2612 <dkhairi50@gmail.com>
2025-05-16 02:41:09 +0800

    Delete app/page directory

--- app/page/.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: '2rem', textAlign: 'center' }}>
-      <h1>Selamat datang, Juniper!</h1>
-      <p>Project-mu berhasil tampil 🎉</p>
-    </div>
-  );
-}
